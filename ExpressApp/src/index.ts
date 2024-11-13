a20ed05bad674625325cf2583c30a10af30ea045
LamaShin <c.blairon-lacoste@hotmail.fr>
2024-11-14 00:40:26 +0100

    ItsWorking

--- ExpressApp/src/index.ts
+++ ExpressApp/src/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/weapons', (req: Request, res: Response) => {
     res.send('Hello, TypeScript avec Express!');
 });
 
